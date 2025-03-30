cf111aa762da668e87a8ec4757d756d2b1ca0970
skyc4ff3ine <skyc4ff3ine@virgilio.it>
2025-03-30 08:30:15 +0200

    updated tailwind and astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });

f1b3c9be51d0463d68e292ca3211b2ca7c57695a
skyc4ffe1ne <skyc4ff3ine@virgilio.it>
2025-06-15 18:57:11 +0200

    Changes

---
+++ src/scripts/animation.js
@@ -0,0 +1,16 @@
+import { gsap } from "gsap";
+
+const cont_logo = document.querySelector(".cont_logo");
+const asterisk = document.querySelector(".asterisk");
+const tl = gsap.timeline({ paused: true });
+
+tl.to(asterisk, {
+  rotate: 360,
+  duration: 1,
+  ease: "bounce.out",
+});
+cont_logo.addEventListener("mouseenter", function() {
+  tl.restart();
+});
+
+console.log("Asterisk: ", asterisk);
